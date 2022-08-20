3c6eaa3ae237c237a464c7403cfd551661987624
Jeremiah Bright <1382636+jbeez@users.noreply.github.com>
2022-08-20 06:30:21 -0400

    Upload pfx cert bundle to imperva site
    Use this to leverage the Imperva API to upload a PFX certificate, work in progress

---
+++ add_pfx_cert.py
@@ -0,0 +1,25 @@
+import requests
+import base64
+
+#site specific
+site_id = {123456}
+pfxfile = "certs\\certfile.pfx"
+pw = "certificatePWhere"
+
+############################################################
+
+with open(pfxfile, "rb") as mycert:
+   b64c = base64.b64encode(mycert.read()).decode('utf-8')
+    
+from apikey import headers
+
+payload = {
+  "certificate": b64c,
+  "passphrase": pw,
+  "auth_type": "RSA"
+}
+
+for id in site_id:
+  url = f"https://my.imperva.com/api/prov/v2/sites/{id}/customCertificate"
+  response = resquests.request("PUT", url, headers=headers, json=payload)
+  print(response.txt
